2c5d89c820793f1a46ba3f510afde31bb7a8d3e8
KantoCanto <bruno.canto.neves@gmail.com>
2022-06-26 18:30:49 +0100

    Root route and Work route working as well as base functionalities of the list

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -5,7 +5,8 @@ const bodyParser = require("body-parser")
 const port = 3000;
 
 //vars
-var items = ["Buy Food", "Cook Food", "Eat Food"];
+let items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
 
 //create app
 const app = express();
@@ -15,31 +16,49 @@ app.set("view engine", "ejs");
 
 //setup body-parser
 app.use(bodyParser.urlencoded({extended: true}));
+//route to public folder
+app.use(express.static("public"));
 
 app.listen(port, function(){
     console.log("Server started on port " + port)
 })
 
 app.get("/", function (req,res){
-    
-    var today = new Date();
+
+    let today = new Date();
    
-    var options = { 
+    let options = { 
         weekday: "long", 
-        year: "numeric", 
         month: "long", 
         day: "numeric"
     };
 
-    var day = today.toLocaleDateString("en-US", options);
+    let day = today.toLocaleDateString("en-US", options);
 
-    res.render("list", {kindOfDay: day, newListItems: items});
+    res.render("list", {listTitle: day, newListItems: items});
 })
 
 app.post("/", function(req, res){
-    var item = req.body.newItem;
-    items.push(item);
+    let item = req.body.newItem;
 
-    res.redirect("/");
+    if (req.body.list === "Work"){
+        workItems.push(item);
+        res.redirect("/work");
+    } else {
+        items.push(item);
+        res.redirect("/");
+    }
+
+   
 })
 
+app.get("/work", function(req, res){
+    res.render("list", {listTitle: "Work List", newListItems: workItems});
+})
+
+app.post("/work", function(req, res){
+    let item = req.body.newItem;
+    workItems.push(item);
+
+    res.redirect("/work");
+})
